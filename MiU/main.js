b76695c4c0c0a4ad77706ff8f717c408b39cdb80
Derek Bertubin <dbertubin@fullsail.edu>
2012-01-06 19:27:25 -0500

    bad committing - this is almost final

--- MiU/main.js
+++ MiU/main.js
@@ -178,22 +178,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 
 	function autoFillData () {
-            var json = {
-                "task1" : {
-                        "groups" : ["Group:" , "A"],
-                        "taskName" : ["Task Name:", "Test App"],
-                        "taskLength" : ["Task Length:", "30"],
-                        "completeBy" : [ "Complete By:", "2011-12-12"],
-                        "notes": ["Notes:", "Notes go here!"]    
-                } ,
-                "task2" : {
-                        "groups" : ["Group:" , "B"],
-                        "taskName" : ["Task Name:", "Test Clear Data"],
-                        "taskLength" : ["Task Length:", "2"],
-                        "completeBy" : [ "Complete By:", "2011-12-12"],
-                        "notes": ["Notes:", "Notes go here again!"]   
-                }  
-            };
+           
             // Store the Json object in to LS
             for ( var n in json) {
                 var id 		= Math.floor(Math.random()*1000000001);
