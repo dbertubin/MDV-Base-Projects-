f32c66b27ca0db9aae2b083e09533cd7cf7d297b
Derek Bertubin <dbertubin@fullsail.edu>
2012-10-05 18:46:40 -0400

    cleaned up display

--- AOC1/Week 2/testApp/testApp/ViewController.m
+++ AOC1/Week 2/testApp/testApp/ViewController.m
@@ -21,7 +21,7 @@
     
     // Set BG color and used http://www.touch-code-magazine.com/web-color-to-uicolor-convertor/ this was used for all color conversions below
         
-    self.view.backgroundColor = [UIColor colorWithRed:0.541 green:0.537 blue:0.561 alpha:1]; /*#8a898f*/
+    self.view.backgroundColor = [UIColor colorWithRed:0.361 green:0.345 blue:0.361 alpha:1]; /*#5c585c*/
 }
 
 - (void) viewWillAppear:(BOOL)animated{
@@ -45,7 +45,7 @@
     // Author Name
     UILabel * author = [[UILabel alloc] initWithFrame:CGRectMake(165.0f, 40.0f, 145.0f, 20.0f)];
     if (author != nil) {
-        author.backgroundColor = [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
+        author.backgroundColor = [UIColor colorWithRed:0.361 green:0.345 blue:0.361 alpha:1]; /*#5c585c*/
         author.text = @" Redfield, James";
         author.textAlignment = UITextAlignmentLeft;
         
@@ -60,31 +60,31 @@
     // Published Date
     UILabel * publishedDate = [[UILabel alloc] initWithFrame:CGRectMake(165.0f, 70.0f, 145.0f, 20.0f)];
     if (publishedDate != nil) {
-        publishedDate.backgroundColor = [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
+        publishedDate.backgroundColor = [UIColor colorWithRed:0.361 green:0.345 blue:0.361 alpha:1]; /*#5c585c*/
         publishedDate.text = @" 1993";
         publishedDate.textAlignment = UITextAlignmentLeft;
     }
     // Summary Label
-    UILabel * summaryLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 100.0f, 145.0f, 20.0f)];
+    UILabel * summaryLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 100.0f, 300.0f, 20.0f)];
     if (summaryLabel != nil) {
         summaryLabel.backgroundColor= [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
-        summaryLabel.text = @"Summary: ";
+        summaryLabel.text = @" Summary: ";
         summaryLabel.textAlignment = UITextAlignmentLeft;
     }
     // Summary
-    UILabel * summary = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 130.0f, 300.0f, 250.0f)];
+    UILabel * summary = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 120.0f, 300.0f, 250.0f)];
     if (summary != nil) {
-        summary.backgroundColor= [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
+        summary.backgroundColor= [UIColor colorWithRed:0.725 green:0.718 blue:0.749 alpha:1]; /*#b9b7bf*/
         summary.text =@"The story of the Celestine Prophesy tells about a man’s adventure into the synchronicities of life. While on this journey, the reluctant main character, John, encounters the 9 Insights, each giving him a deeper understanding and experience in his own spiritual growth.The story is packed with chases, near death experience,revelation and awakening. It is sure to keep you on the edge of your seat and centered in your soul.";
         summary.textAlignment = UITextAlignmentCenter;
         summary.numberOfLines = 30;
     }
     
     // List Label
-    UILabel * listLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 390.0f, 300.0f, 30.0f)];
+    UILabel * listLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 380.0f, 300.0f, 20.0f)];
     if (listLabel != nil) {
         listLabel.backgroundColor = [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
-        listLabel.text = @"List of Topics";
+        listLabel.text = @" List of Topics";
         listLabel.textAlignment = UITextAlignmentLeft;
     }
     // Create NSArray and pass thru objects thru mutableString using a for loop
@@ -99,9 +99,9 @@
     }
     
     // List field with dynamic list from mutableString 
-    UILabel * list = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 420.0f, 300.0f, 45.0f)];
+    UILabel * list = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 400.0f, 300.0f, 45.0f)];
     if (list != nil) {
-        list.backgroundColor = [UIColor colorWithRed:0.992 green:0.729 blue:0.11 alpha:1]; /*#fdba1c*/
+        list.backgroundColor = [UIColor colorWithRed:0.725 green:0.718 blue:0.749 alpha:1]; /*#b9b7bf*/
         list.text = mutableString;
         list.textAlignment = UITextAlignmentCenter;
         list.numberOfLines = 2;
