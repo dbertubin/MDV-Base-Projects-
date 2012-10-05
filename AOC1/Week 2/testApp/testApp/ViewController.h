54de285cca2f2c0991659dea6e072003f53423e7
Derek Bertubin <dbertubin@fullsail.edu>
2012-10-04 22:05:39 -0400

    Added Week 2

---
+++ AOC1/Week 2/testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Derek Bertubin on 10/4/12.
+//  Copyright (c) 2012 Derek Bertubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOC1/Week 2/testApp/testApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testApp
+//
+//  Created by Derek Bertubin on 10/4/12.
+//  Copyright (c) 2012 Derek Bertubin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
