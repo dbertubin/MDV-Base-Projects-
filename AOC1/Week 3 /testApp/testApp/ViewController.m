d9e30aa30c34effededf578ed086f667a20ac5cd
Derek Bertubin <dbertubin@fullsail.edu>
2012-10-10 00:26:55 -0400

    Added Instructions to Week 3 Project

---
+++ AOC1/Week 3 /testApp/testApp/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  testApp
+//
+//  Created by Derek Bertubin on 10/6/12.
+//  Copyright (c) 2012 Derek Bertubin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    
+//*****For this week's project, you will be taking what you've learned of NSNumbers, functions and alerts.******
+    
+//  1.  Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two
+    
+//  2.  Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
+    
+//  3.  Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
+    
+//  4.  Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    
+//  5.  Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
+    
+//  6.  Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    
+//  7.  Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    
+//  8.  Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+    
+//  9.  Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
+    
+//*****DisplayAlertWithString Function*****
+    
+//  1. Take the passed in NSString and display it in the alert view
+    
+//  2. Create an UIAlertView
+
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AOC1/Week 3 /testApp/testAppTests/testAppTests.h
@@ -0,0 +1,13 @@
+//
+//  testAppTests.h
+//  testAppTests
+//
+//  Created by Derek Bertubin on 10/6/12.
+//  Copyright (c) 2012 Derek Bertubin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testAppTests : SenTestCase
+
+@end
